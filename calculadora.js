91a410cc84cac5cf0d525448c1a0037ec8b24348
pititinga <118141152+pititinga@users.noreply.github.com>
2022-11-15 21:27:18 -0300

    improvig and cleaning the code

--- calculadora.js
+++ calculadora.js
@@ -1,26 +1,23 @@
-function insert(num)
-{
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+function insert(num) {
+    var number = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = number + num;
 }
-function clean()
-{
-    document.getElementById('resultado').innerHTML = "";
+
+function clean() {
+    document.getElementById('result').innerHTML = "";
 }
-function back()
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+function back() {
+    var result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length - 1);
 }
-function calcular()
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
-    }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "nada..."
+
+function calculate() {
+    var result = document.getElementById('result').innerHTML;
+
+    if (result) {
+        document.getElementById('result').innerHTML = eval(result);
+    } else {
+        document.getElementById('result').innerHTML = 0;
     }
 }
